b28fd20e0d1dd3ca13f32f80bb04d38a587b392a
sxv <102722283+sxv1101@users.noreply.github.com>
2025-08-10 11:07:09 +0800

    Added test cases and code coverage with vitest

---
+++ vitest.workspace.js
@@ -0,0 +1,27 @@
+import { defineWorkspace } from "vitest/config";
+
+export default defineWorkspace([
+  {
+    extends: "./vite.config.js",
+    test: {
+      include: ["**/*.node.test.{js,jsx}"],
+      name: "happy-dom",
+      environment: "happy-dom",
+      coverage: ["text", "json", "html"],
+    },
+  },
+  {
+    extends: "./vite.config.js",
+    test: {
+      setupFiles: ["vitest-browser-react"],
+      include: ["**/*.browser.test.{js,jsx}"],
+      name: "browser",
+      coverage: ["text", "json", "html"],
+      browser: {
+        provider: "playwright",
+        enabled: true,
+        name: "firefox",
+      },
+    },
+  },
+]);
